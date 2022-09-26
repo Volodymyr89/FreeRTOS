eab28d5421e95790fa6392724a9a15fb4dbab2d9
VolB <43251877+Volodymyr89@users.noreply.github.com>
2022-09-26 14:02:52 +0300

    queues managin

--- main/real_time_stats_example_main.c
+++ main/real_time_stats_example_main.c
@@ -13,58 +13,46 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "esp_err.h"
+typedef enum structDataID{
+    I2C_ID,
+    SPI_ID
+}ID_t;
 
-char value1[50] = "I'm from Task1";
-char value2[50] = "I'm from Task2";
-char value3[50] = "I'm from Task3";
+typedef struct{
+    ID_t DataID;
+    uint32_t Value;
+}data_t;
 
 TaskHandle_t xTaskhandle1 = NULL;
 TaskHandle_t xTaskhandle2 = NULL;
-TaskHandle_t xTaskhandle3 = NULL;
 QueueHandle_t xQueue = NULL;
 
 static void Task1(void *arg)
 {
-    char *s = (char *) arg;
-    //Print real time stats periodically
-    while (1) {
-        printf("Task1 is running %s\n", s);
-        if(xQueue != pdFALSE){
-          xQueueSendToBack(xQueue, (char*) s, 100);
-        }
-          else{
-            printf("ERROR\n");
-          }
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    data_t *data = (data_t *) arg;
+    data_t *receive = NULL;
+    xQueueSend(xQueue, data, 100);
+    xQueueReceive(xQueue, receive, 100);
+    printf("Data received from %s and Value is %d", receive->DataID, receive->Value);
+ 
 }
 
 static void Task2(void *arg)
 {
-    char *s = (char*) arg;
-    //Print real time stats periodically
-    while (1) {
-        printf("Task2 is running  %s\n", s);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    data_t *data = (data_t *) arg;
+    xQueueSend(xQueue, data, 100);
 }
 
-static void Task3(void *arg)
-{
-    char *s = (char*) arg;
-    xQueueReceive(xQueue, s, 10);
-    while (1) {
-        printf("Task3 is running  %s\n", s);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
 
 void app_main(void)
 {
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    xQueue = xQueueCreate(1, (sizeof(char)*50));
-    //Create and start stats task
-    xTaskCreatePinnedToCore(Task1, "Task1", 4096, value1, 1, &xTaskhandle1, 0);
-    xTaskCreatePinnedToCore(Task2, "Task2", 4096, value2, 1, &xTaskhandle2, 0);
-    xTaskCreatePinnedToCore(Task3, "Task3", 4096, value3, 1, &xTaskhandle2, 0);
+    const data_t dataID[2] = {
+        { .DataID = I2C_ID, .Value = 10},
+        { .DataID = SPI_ID, .Value = 20}
+    };
+    xQueue = xQueueCreate(2, sizeof(dataID));
+    if(xQueue !=NULL){
+        xTaskCreatePinnedToCore(Task1, "Task1", 4096, &(dataID[0]), 1, &xTaskhandle1, 0);
+        xTaskCreatePinnedToCore(Task2, "Task2", 4096, &(dataID[1]), 2, &xTaskhandle2, 0);
+    }
 }
